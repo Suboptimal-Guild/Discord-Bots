a01c59793426821140da682f0161ee68d43c30eb
ihanken <ihanken@bellsouth.net>
2016-11-09 22:43:08 -0600

    I'm a fuckboy

--- discord_bot.py
+++ discord_bot.py
@@ -1,16 +1,60 @@
 #!/usr/bin/python
-
+'''
 import discord
-
-client = discord.Client()
+import asyncio
 
 # Disables the SSL warning that is printed to the console.
 import requests.packages.urllib3
 requests.packages.urllib3.disable_warnings()
 
-@client.event
+def example_func(author, message):
+    client.send_message(message.channel, "%s, How are you doing?" % author)
 
+#@client.event
 def on_message(message):
+    author = message.author
 
-    hashedPW = b'$2b$12$e.iRNKBzIpx7cDkzIdR8s.7Zce7bSef4dBnVDHcRj7QVBgLlXCEY2';
-    realPW = 
+    if message.content.startwith('!test'):
+        example_func(author, message)
+
+
+
+password = 'MjQzMTIzNjEyNTMyMzQyNzg5.CwWINA.Asi_1i6v3MUvBQfiqg4tFD0tnWs'
+ID = 'starter-pot#3689'
+
+client.login(ID, password)
+
+client.accept_invite('https://discord.gg/b9T4n')
+
+client.run()
+'''
+
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'I\'m a fuckboy.\n Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.accept_invite('https://discord.gg/mM5fXCe')
+
+client.run('MjQ2MTMxMjkyMjg5MjM2OTky.CwWLNA.Un3vOVd-WKZxpQpTHswfd1ozJUk')
